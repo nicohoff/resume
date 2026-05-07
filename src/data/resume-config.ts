441d6ec617473d2ee44c6f8c8409eafaa58538b8
nicoff-dev <nicoff.dev@gmail.com>
2026-05-07 17:35:07 +0200

    Update resume-config.ts

--- src/data/resume-config.ts
+++ src/data/resume-config.ts
@@ -95,7 +95,7 @@ export const resumeConfig: ResumeConfig = {
       company: { fr: 'Bouygues Energies & Services' },
       role: { fr: 'Développeur SCADA' },
       type: { fr: 'CDI' },
-      period: { fr: 'Depuis 2021'},
+      period: { fr: '2021 - ...'},
       description: {
         fr: 'Gestion d\'infrastructures : tunnels routiers / autoroutiers, GTB',
       },
